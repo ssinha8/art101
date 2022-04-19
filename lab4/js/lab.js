fdb7d2a2be39f37a7632be118fb4a195ba76bcef
ssinha8 <ssinha8@ucsc.edu>
2022-04-19 05:26:17 -0700

    Lab 4 Final

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+//  This contains pseudcode for an interactive to-do list=.
+// Interactive to-do list.
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Shloak Sinha, Phil Meyers.
+// @since  x.x.x
+
+// Creating a to-do list:
+  // Print out the list
+  // Accept user input
+  // If input is add item, add item to list
+  // if input is cross item, cross out item on list
+  // If input is remove item, remove item from list
+  // Update list and go back to top
