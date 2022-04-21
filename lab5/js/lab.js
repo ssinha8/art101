8b9ca92cec9c1247dd71a16a7a33ef43e5844871
ssinha8 <ssinha8@ucsc.edu>
2022-04-20 23:45:24 -0700

    Finished Lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/**
+ * Author:    Shloak Sinha
+ * Created:   04/20/2022
+ *
+ * School: UCSC
+ * Class: Art101
+ **/
+
+// Defining variables
+make = "Volvo";
+model = "XC60";
+color = "Midnight Blue";
+year = 2015;
+ownIt = true;
+
+// Calculate age of car
+age = 2022 - year;
+
+// Printing variables
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
