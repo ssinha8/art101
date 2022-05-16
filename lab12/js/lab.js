268cbbbfd4035382b890e2e419c237c8d7b0c556
ssinha8 <ssinha8@ucsc.edu>
2022-05-16 10:16:28 -0700

    basics done -- setting up TaskX

---
+++ lab12/js/lab.js
@@ -0,0 +1,43 @@
+/**
+ * Author:    Shloak Sinha
+ * Created:   05/16/2022
+ *
+ * School: UCSC
+ * Class: Art101
+ **/
+
+// array for Hogwarts Houses
+
+const houses = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff'];
+
+
+// sortingHat function
+
+function sortingHat(str) {
+  mod = str.length % 4;
+
+  // if (mod == 0) {
+  //   return "Gryffindor";
+  // } else if (mod == 1) {
+  //   return "Ravenclaw";
+  // } else if (mod == 2) {
+  //   return "Slytherin";
+  // } else {
+  //   return "Hufflepuff";
+
+  return houses[mod];
+}
+
+// button initialization
+var button = document.getElementById("button");
+
+
+// listener for the button that enables the click function
+button.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+
+  var newText =  "<p>The Sorting Hat has sorted you into " + house + "</p>";
+
+  document.getElementById('output').prepend(newText);
+})
