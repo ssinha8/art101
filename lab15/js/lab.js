8e544e40fe0ffc70bb835cb164b113dbb1fb96f6
Shloak Sinha <ssinha8@ucsc.edu>
2022-05-27 00:23:43 -0700

    Update lab.js

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -6,4 +6,32 @@
  * Class: Art101
  **/
 
- 
+ var button = document.getElementById("activate");
+
+ // Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://yesno.wtf/#no",
+    // The data to send (will be converted to a query string)
+    data: {
+            // here is where any data required by the api
+            //   goes (check the api docs)
+            id: 123,
+            api_key: "blahblahblah",
+          },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        console.log(data);
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+})
