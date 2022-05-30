f8af4e60c50c029f8895e4d67e0cf884dbed6e90
Shloak Sinha <ssinha8@ucsc.edu>
2022-05-29 18:56:37 -0700

    finished lab15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -6,32 +6,31 @@
  * Class: Art101
  **/
 
- var button = document.getElementById("activate");
-
+$("#activate").on('click', function() {
  // Using the core $.ajax() method
-$.ajax({
-    // The URL for the request (from the api docs)
-    url: "https://yesno.wtf/#no",
-    // The data to send (will be converted to a query string)
-    data: {
-            // here is where any data required by the api
-            //   goes (check the api docs)
-            id: 123,
-            api_key: "blahblahblah",
-          },
-    // Whether this is a POST or GET request
-    type: "GET",
-    // The type of data we expect back
-    dataType : "json",
-    // What do we do when the api call is successful
-    //   all the action goes in here
-    success: function(data) {
-        // do stuff
-        console.log(data);
-    },
-    // What we do if the api call fails
-    error: function (jqXHR, textStatus, errorThrown) {
-        // do stuff
-        console.log("Error:", textStatus, errorThrown);
-    }
-})
+  $.ajax({
+      // The URL for the request (from the api docs)
+      url: "https://pokeapi.co/api/v2/berry/",
+      // The data to send (will be converted to a query string)
+      data: {
+              // here is where any data required by the api
+              //   goes (check the api docs)
+              id: 4,
+            },
+      // Whether this is a POST or GET request
+      type: "GET",
+      // The type of data we expect back
+      dataType : "json",
+      // What do we do when the api call is successful
+      //   all the action goes in here
+      success: function(data) {
+          // do stuff
+          $("#output").html(JSON.stringify(data));
+      },
+      // What we do if the api call fails
+      error: function (textStatus, errorThrown) {
+          // do stuff
+          console.log("Error:", textStatus, errorThrown);
+      }
+  })
+});
